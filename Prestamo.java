6bc902b9bef50fedd940d87f2ce71adb116b4abf
izarbe <izarbelanuza06@gmail.com>
2026-04-17 19:39:33 +0200

    prestamo iniciado

---
+++ Prestamo.java
@@ -0,0 +1,28 @@
+import java.time.LocalDate;
+
+public class Prestamo {
+
+    private Usuario usuario;
+    private Libro libro;
+    private LocalDate fechaPrestamo;
+    private LocalDate fechaDevolucion;
+
+    public Prestamo(Usuario usuario, Libro libro, LocalDate fechaPrestamo) {
+        this.usuario = usuario;
+        this.libro = libro;
+        this.fechaPrestamo = fechaPrestamo;
+        this.fechaDevolucion = fechaPrestamo.plusWeeks(2);
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public Libro getLibro() {
+        return libro;
+    }
+
+    public LocalDate getFechaDevolucion() {
+        return fechaDevolucion;
+    }
+}
