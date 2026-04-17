6efd3dd5ed7d20b7f19ba7791e9e991be3bcadb8
izarbe <izarbelanuza06@gmail.com>
2026-04-17 19:30:25 +0200

    iniciados usuario y gestorBiblioteca

---
+++ GestorBiblioteca.java
@@ -0,0 +1,27 @@
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+public class GestorBiblioteca {
+
+    private List<Prestamo> prestamos;
+
+    public GestorBiblioteca() {
+        this.prestamos = new ArrayList<>();
+    }
+
+    public void prestarLibro(Usuario usuario, Libro libro) {
+        if (!libro.estaPrestado()) {
+            libro.prestar();
+
+            Prestamo prestamo = new Prestamo(usuario, libro, LocalDate.now());
+            prestamos.add(prestamo);
+
+            usuario.agregarLibro(libro);
+
+            System.out.println(usuario.getNombre() + " ha cogido el libro: " + libro.getTitulo());
+        } else {
+            System.out.println("Este libro ya está prestado.");
+        }
+    }
+}
--- Usuario.java
+++ Usuario.java
@@ -3,29 +3,27 @@ import java.util.List;
 
 public class Usuario {
 
-	private String nombre;
-	private List<Libro> librosPrestados;
+    private String nombre;
+    private List<Libro> librosPrestados;
 
-	public Usuario(String nombre) {
-		this.nombre = nombre;
-		this.librosPrestados = new ArrayList<>();
-	}
+    public Usuario(String nombre) {
+        this.nombre = nombre;
+        this.librosPrestados = new ArrayList<>();
+    }
 
-	public String getNombre() { 
-		return nombre; 
-	}
+    public String getNombre() {
+        return nombre;
+    }
 
-	public List<Libro> getLibrosPrestados() { 
-		return librosPrestados; 
-	}
+    public List<Libro> getLibrosPrestados() {
+        return librosPrestados;
+    }
 
-	public void añadirLibro(Libro libro) { 
-		librosPrestados.add(libro); 
-	}
-	public void devolverLlibro(Libro libro) { 
-		librosPrestados.remove(libro); 
-	}
+    public void agregarLibro(Libro libro) {
+        librosPrestados.add(libro);
+    }
 
-
-
+    public void devolverLibro(Libro libro) {
+        librosPrestados.remove(libro);
+    }
 }
