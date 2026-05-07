f053caf7b599dca0cff07fab663de80d4dc3bae4
Izarbe.l <izarbelanuza06@gmail.com>
2026-05-07 18:54:27 +0200

    subo src y .java

---
+++ src/biblioteca/Libro.java
@@ -0,0 +1,52 @@
+package biblioteca;
+public class Libro {
+
+	private String titulo;
+	private String autor;
+	private boolean prestado;
+
+	public Libro(String titulo, String autor) {
+		this.titulo = titulo;
+		this.autor = autor;
+		this.prestado = false;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public String getAutor() {
+		return autor;
+	}
+
+	public boolean estaPrestado() {
+		return prestado;
+	}
+
+	public void prestar() {
+		prestado = true;
+	}
+
+	public void devolver() {
+		prestado = false;
+	}
+
+	public boolean disponibilidadLibro() {
+		
+		
+		System.out.print("El libro " + titulo );
+		
+		if(prestado) {
+			System.out.println(" no esta disponible...");  
+			return false;
+			
+		} else {
+			System.out.println(" esta disponible.");
+			return true;
+		}
+	}
+	@Override
+	public String toString() {
+		return titulo + " de " + autor + (prestado ? " (Prestado)" : " (Disponible)");
+	}
+}
